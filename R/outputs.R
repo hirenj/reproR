c557b35b33babe2d133c20be3d333ec95d3d9b06
Hiren Joshi <hirenj@gmail.com>
2015-10-06 16:37:49 +0200

    Add in links to github

--- R/outputs.R
+++ R/outputs.R
@@ -1,3 +1,21 @@
+parseRemote <- function(url,sha) {
+	url_components = strsplit(url,'/')
+	lapply(url_components,function(components) {
+		if ("github.com" %in% components) {
+			return(paste("https://github.com/",components[4],"/",gsub('\\.git','',components[5]),"/commit/",sha,sep=""))
+		}
+		if (length(grep("@github.com",components)) > 0) {
+			return(paste("https://github.com/",gsub(".*:","",components[1]),"/",gsub('\\.git','',components[2]), "/commit/",sha,sep="" ))
+		}
+		if ("gist.github.com" %in% components) {
+			return(paste("https://gist.github.com/",gsub('\\.git','',components[4]),"/",sha,sep=""))
+		}
+		if (length(grep("@gist.github.com",components)) > 0) {
+			return(paste("https://gist.github.com/",gsub( '\\.git','',gsub(".*:","",components[1])),"/",sha,sep="" ))
+		}
+	})
+}
+
 #' Generate markdown
 #' @export
 status.md <- function(session=F) {
@@ -6,5 +24,6 @@ status.md <- function(session=F) {
 	if (session) {
 	  sessionText='sessionInfo()'
 	}
-	return(paste('## Commit ',current_commit,' \n```{r}\n',sessionText,'\n```\n\n<object type="application/pdf" data="file://changed.patch" data-attachment="patchfile.patch" ></object>',sep=''))
+	remote_url = parseRemote(git2r::remote_url(repo()),current_commit)
+	return(paste('## Commit ',current_commit,'\n [',current_commit,'](',remote_url,')\n\n```{r}\n',sessionText,'\n```\n\n<object type="application/pdf" data="file://changed.patch" data-attachment="patchfile.patch" ></object>',sep=''))
 }
